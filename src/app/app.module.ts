791410e747cf4657fb88be647490cd1eec5e09c6
jorge sanabria <jorge.adrian.sanabria@gmail.com>
2019-09-08 11:05:21 -0300

    arreglos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 // modulos
 import { PageModule } from './pages/pages.module';
 
-import { AppRoutingModule } from './app-routing.module';
+import { APP_ROUTES } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
@@ -18,7 +18,7 @@ import { RegisterComponent } from './login/register.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    APP_ROUTES,
     PageModule
   ],
   providers: [],
