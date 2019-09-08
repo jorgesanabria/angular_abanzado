f7b531e4d3d743b23e42d9efb227cc8f12faf785
jorge sanabria <jorge.adrian.sanabria@gmail.com>
2019-09-08 10:38:00 -0300

    trabajando en rutas

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModele } from '../shared/shared.module';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+import { PAGE_ROUTES } from './pages.routes';
+
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        SharedModele,
+        PAGE_ROUTES
+    ]
+})
+export class PageModule { }
